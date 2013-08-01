167deaf235e7647d512961bdc646b21219f299c6
Brian Caicco <briancaicco@gmail.com>
2013-08-01 12:22:37 -0400

    theme switch

--- structure/themes/base-theme/index.php
+++ structure/themes/base-theme/index.php
@@ -6,5 +6,6 @@ echo $page->get_header();
 ?>
 <? include 'partials/slider.php'; ?>
 <? include 'partials/action-banner.php'; ?>
-<? include 'partials/2-columns.php'; ?>
+<? include 'partials/3-columns.php'; ?>
+<? include 'partials/action-banner.php'; ?>
 <? echo $page->get_footer();?>
