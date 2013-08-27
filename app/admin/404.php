f87ac02eaa527a4f481c02ffab6316fcaad30730
Brian Caicco <briancaicco@gmail.com>
2013-08-27 18:45:24 -0400

    custom 404

---
+++ app/admin/404.php
@@ -0,0 +1,14 @@
+<?php
+$pagetitle = "404 - Content Not Found";
+include 'header.php'
+?>
+
+<div id="content">
+	<div class="col-12">
+		<h1>Keep Diggin'</h1>
+	</div>
+</div>
+
+<?php 
+include 'footer.php'
+?>
