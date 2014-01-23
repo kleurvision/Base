432c4c034daef394127ac2ba9b52f85838f909a2
Brian Caicco <briancaicco@gmail.com>
2014-01-23 18:38:33 -0500

    theme tweaks

---
+++ app/themes/rosedale/page-portfolio.php
@@ -0,0 +1,26 @@
+<? 
+/* 
+    Template Name: Portfolio
+ */
+
+echo $page->get_header();
+?>
+
+<section class="page">
+    <div class="page-header">
+        <div class="container">
+            <div class="col-12 col-lg-6">
+                <h2 class="page-title"><?= $pagemap->pagetitle;?></h2>
+            </div>        
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-lg-12">
+                <p><?= $pagemap->pagecontent;?></p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<? echo $page->get_footer();?>
